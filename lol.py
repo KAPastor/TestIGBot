1a065e8197f58e621e6dca5e9ce2f0b09626e1da
Kyle Pastor <kyleanthonypastor@gmail.com>
2018-06-01 12:36:59 -0400

    sdfsfsd
    sdf

---
+++ lol.py
@@ -0,0 +1,19 @@
+import smtplib
+
+# you == the recipient's email address
+msg['Subject'] = 'The contents of '
+msg['From'] = 'kyleanthonypastor@gmail.com'
+msg['To'] = 'kyle.pastor@scotiabank.com'
+
+fromaddr = 'kyleanthonypastor@gmail.com'
+toaddrs  = 'kyle.pastor@scotiabank.com'
+msg = 'Why,Oh why!'
+username = 'kyleanthonypastor@gmail.com'
+password = 'Skiptime12345!@#$%'
+
+server = smtplib.SMTP('smtp.gmail.com:587')
+server.ehlo()
+server.starttls()
+server.login(username,password)
+server.sendmail(fromaddr, toaddrs, msg)
+server.quit()
